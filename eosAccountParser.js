cba0b9df8f4e3dde26fa4b462b116693e4cf76ed
abasalilov <abasalilov@gmail.com>
2018-08-25 16:16:36 -0700

    float int parse added

--- eosAccountParser.js
+++ eosAccountParser.js
@@ -1,7 +1,38 @@
+var fs = require("fs");
 const data = require("./data/eosAccounts.json");
 
-let counter = 0;
+const parsedNames = [];
 
-for (i = 0; i < 1000; i++) {
-  console.log(data[i].account_name);
+const writeJSONFile = dataArr => {
+  const stringDataArr = JSON.strigify(dataArr);
+  fs.writeFile("eosAccounts.json", stringDataArr, err => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log("EOS docs saved!");
+  });
+};
+
+const parseEOSaccountName = account => {
+  const { account_name, total_eos } = account;
+  const totalInt = parseInt(total_eos);
+  const totalFloat = parseFloat(total_eos);
+  // console.log("account.account_name", account_name);
+  console.log("total", totalInt);
+  console.log("type", totalFloat);
+  let total = totalInt > totalFloat ? totalInt : totalFloat;
+  if (total > 0.1) {
+    return true;
+  }
+};
+
+for (i = 0; i < 3; i++) {
+  let account = data[i];
+  let bool = parseEOSaccountName(account);
+  if (bool) {
+    parsedNames.push(account.account_name);
+  }
 }
+
+console.log("parsedNames", parsedNames);
